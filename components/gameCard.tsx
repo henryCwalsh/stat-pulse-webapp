d6d022fad9a5322062ec905e99fed067ec3a92ca
Henry Walsh <henrywalshba@gmail.com>
2025-08-11 17:16:25 -0400

    generic score cards with winner specific styles

--- components/gameCard.tsx
+++ components/gameCard.tsx
@@ -1,5 +1,6 @@
 import { sampleGamesData } from "@/data/gameSampleData";
 import { Game } from "@/types/nba";
+import { useState } from "react";
 
 type GameCardProps = {
   game: Game;
@@ -7,5 +8,15 @@ type GameCardProps = {
 
 
 export default function GameCard({ game }: { game: any }) {
-  return <div>{game.teams.home.name} vs {game.teams.visitors.name}</div>;
+  const homePoints = game.scores.home.points;
+  const visitorPoints = game.scores.visitors.points;
+
+  const homeWinner = homePoints > visitorPoints;
+
+
+
+  return <div key={game.id} className="border border-white max-w-md mx-auto bg-gray-900 rounded-lg shadow-lg p-6 text-gray-400 font-sans">
+    <div className={homeWinner ? "text-white " : ""}>{game.teams.home.name}: {game.scores.home.points}</div>
+    <div className={!homeWinner ? "text-white " : ""}>{game.teams.visitors.name}: {game.scores.visitors.points}</div>
+    </div>;
 }
