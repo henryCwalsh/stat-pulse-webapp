d0644ad606084cedb251e3f71d92cc293a673ef2
Henry Walsh <henrywalshba@gmail.com>
2025-08-11 20:14:05 -0400

    scorecards v1 with logos complete

--- components/gameCard.tsx
+++ components/gameCard.tsx
@@ -1,5 +1,6 @@
 import { sampleGamesData } from "@/data/gameSampleData";
 import { Game } from "@/types/nba";
+import Image from "next/image";
 import { useState } from "react";
 
 type GameCardProps = {
@@ -15,8 +16,40 @@ export default function GameCard({ game }: { game: any }) {
 
 
 
-  return <div key={game.id} className="border border-white max-w-md mx-auto bg-gray-900 rounded-lg shadow-lg p-6 text-gray-400 font-sans">
-    <div className={homeWinner ? "text-white " : ""}>{game.teams.home.name}: {game.scores.home.points}</div>
-    <div className={!homeWinner ? "text-white " : ""}>{game.teams.visitors.name}: {game.scores.visitors.points}</div>
-    </div>;
+  return (
+    <div key={game.id} className="border border-white max-w-md mx-auto bg-gray-900 rounded-lg shadow-lg p-2 text-gray-400 font-sans">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <Image
+            src={game.teams.home.logo}
+            alt={`${game.teams.home.name} logo`}
+            width={24}
+            height={24}
+          />
+          <span className={homeWinner ? "text-white " : ""}>
+            {game.teams.home.name}
+          </span>
+        </div>
+        <div className={homeWinner ? "text-white " : ""}>
+          {game.scores.home.points}
+        </div>
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <Image
+            src={game.teams.visitors.logo}
+            alt={`${game.teams.visitors.name} logo`}
+            width={24}
+            height={24}
+          />
+          <span className={!homeWinner ? "text-white " : ""}>
+            {game.teams.visitors.name}
+          </span>
+        </div>
+        <div className={!homeWinner ? "text-white " : ""}>
+          {game.scores.visitors.points}
+        </div>
+      </div>
+    </div>
+  );
 }
